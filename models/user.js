bd3242ac9cd4bf9ddf11dde0c580e42d3df658f1
WesleyVeeningen <112481427+WesleyVeeningen@users.noreply.github.com>
2024-04-13 00:30:20 +0200

    update user.js

--- models/user.js
+++ models/user.js
@@ -16,9 +16,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-        token: {
+    token: {
         type: String,
-        required: true
+        required: false
     },
 });
 
